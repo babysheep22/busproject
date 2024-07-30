023f968bac47b87e2a32e89798aaca8f9bd9150a
babysheep22 <90301724+babysheep22@users.noreply.github.com>
2024-07-30 13:28:59 +0900

    버스 프로젝트 시작
    가보자~~~!..

---
+++ busproject3/src/main/java/com/example/busproject3/Busproject3Application.java
@@ -0,0 +1,13 @@
+package com.example.busproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Busproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Busproject3Application.class, args);
+	}
+
+}
---
+++ busproject3/src/test/java/com/example/busproject3/Busproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Busproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
